969ef224f65462c0e962725412141c60b60a543c
abyad-tif <lapajung50@gmail.com>
2025-10-13 14:09:09 +0800

    initial commit + nampilin data

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+import Routes from "./routes";
+
+const app = new Elysia();
+
+app.get("/", () => "Hello Elysia!");
+
+app.group("/api", (app) => app.use(Routes));
+
+app.listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
