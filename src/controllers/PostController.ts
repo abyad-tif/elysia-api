199eb0bfd4fce84d87ef8270f160c07af16fe59d
abyad-tif <lapajung50@gmail.com>
2025-10-13 18:39:22 +0800

    fungsi create data

--- src/controllers/PostController.ts
+++ src/controllers/PostController.ts
@@ -1,5 +1,6 @@
 import prisma from "../../prisma/client";
 
+// Fungsi Show Data
 export async function getPosts() {
   try {
     const posts = await prisma.post.findMany({ orderBy: { id: "desc" } });
@@ -11,5 +12,27 @@ export async function getPosts() {
     };
   } catch (e: unknown) {
     console.error(`Error getting posts: ${e}`);
+  }
+}
+
+// Fungsi Create Data
+export async function createPosts(options: { title: string; content: string }) {
+  try {
+    const { title, content } = options;
+
+    const post = await prisma.post.create({
+      data: {
+        title: title,
+        content: content,
+      },
+    });
+
+    return {
+      success: true,
+      message: "Post Created Successfully!",
+      data: post,
+    };
+  } catch (e: unknown) {
+    console.log(`Error creating post: ${e}`);
   }
 }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,26 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 
-import { getPosts } from "../controllers/PostController";
+import { createPosts, getPosts } from "../controllers/PostController";
 
-const Routes = new Elysia({ prefix: "/posts" }).get("/", () => getPosts());
+const Routes = new Elysia({ prefix: "/posts" })
+
+  .get("/", () => getPosts())
+
+  .post(
+    "/",
+    ({ body }) => createPosts(body as { title: string; content: string }),
+    {
+      body: t.Object({
+        title: t.String({
+          minLength: 3,
+          maxLength: 100,
+        }),
+        content: t.String({
+          minLength: 3,
+          maxLength: 1000,
+        }),
+      }),
+    }
+  );
 
 export default Routes;
